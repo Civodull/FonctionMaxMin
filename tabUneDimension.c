bb56cba1ad144f46a29f7561d76a072fd30c67db
Civodu <ludovendi@gmail.com>
2020-11-12 11:46:19 +0000

    tableau une dimension

--- tabUneDimension.c
+++ tabUneDimension.c
@@ -1,30 +1,32 @@
 #include <stdio.h>
-
+#include<ctype.h>
 
 int main()
 {
-    int i,position, vrai=0,n, tab[4]= {32,1,4,56};
-   char c;
+    int nombreSaisir, tab[4]= {32,1,4,56};
+    int i,position, trouve=0;
+    char c;
+
     printf("Veuillez saisir la valeur a chercher ");
 do
 {
-    if (scanf("%d",&n) == 1)
+    if (scanf("%f",&nombreSaisir) == 1)
     {
         for (i = 0; i < 4; i++)
         {
-            if (tab[i] == n)
+            if (tab[i] == nombreSaisir)
             {
                 position = i;
-                vrai = 1;
-                break;
+                trouve = 1;
+             
             }
         }
-        if (vrai == 1)
+        if (trouve == 1)
         {
-            printf("L'element %d se trouve a la position %d du tableau\n",n,position);
+            printf("L'element %.2f se trouve a la position %d du tableau\n",nombreSaisir,position);
         }else
         {
-            printf("L'element %d n'existe pas dans le tableau\n",n);
+            printf("L'element %.2f n'existe pas dans le tableau\n",nombreSaisir);
         }
     }else
     printf("La valeur que vous avez saisis n'est pas un entier\n\tSaisissez un entier\n");
